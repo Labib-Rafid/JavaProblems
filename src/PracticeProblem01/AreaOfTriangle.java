1d114e54cd90a2a8fe7baa5f6ef5754994fb1244
Mohammed Labib Rafid Majumder <labib.rafid2003@gmail.com>
2025-01-06 20:39:04 +0600

    PracticeProblem01_Push

---
+++ src/PracticeProblem01/AreaOfTriangle.java
@@ -0,0 +1,10 @@
+package PracticeProblem01;
+
+public class AreaOfTriangle {
+    public static void main(String args[]){
+        double base = 20.55;
+        double height = 10.12;
+        double triangleArea = (.5) * base * height;
+        System.out.println("Area of Triangle: " + triangleArea);
+    }
+}
