856772c9087363d9a09f63fa55f9e110636dffc5
Mohammed Labib Rafid Majumder <labib.rafid2003@gmail.com>
2025-01-08 19:06:55 +0600

    PracticeProblem04_UpdateToDate

---
+++ src/PracticeProblem04/PatternPrinting.java
@@ -0,0 +1,17 @@
+package PracticeProblem04;
+
+import java.util.Scanner;
+
+public class PatternPrinting {
+    public static void main(String[] args) {
+        System.out.print("Enter Number of Rows: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        for(int i = 1; i <= n; ++i){
+            for(int j = 1; j <= i; ++j){
+                System.out.print(j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
