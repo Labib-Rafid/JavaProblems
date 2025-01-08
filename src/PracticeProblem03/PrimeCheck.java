95db6648e39966d99ba243046cfdfdbb853c2862
Mohammed Labib Rafid Majumder <labib.rafid2003@gmail.com>
2025-01-08 09:51:04 +0600

    PracticeProblem03_Update

---
+++ src/PracticeProblem03/PrimeCheck.java
@@ -0,0 +1,23 @@
+package PracticeProblem03;
+
+import java.util.Scanner;
+
+public class PrimeCheck {
+    public static void main(String[] args) {
+        System.out.println("Enter The Number: ");
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+
+        boolean flag = false;
+        for(int i = 1; i < n; ++i){
+            if(n % i == 0){
+                flag = true;
+                break;
+            }
+        }
+        if(n == 1) System.out.println("Not prime neither composite");
+        else if(!flag) System.out.println("Prime");
+        else System.out.println("Not Prime");
+    }
+}
